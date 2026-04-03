b08f221092b46e7313ccc1f2470a2be162be661c
Kevin <kevin.so1990@gmail.com>
2026-04-03 19:55:38 +0200

    Fix local Expo startup and restore app entry

--- client/App.tsx
+++ client/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import RootStackNavigator from "@/navigation/RootStackNavigator";
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>FitPlan Test</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <RootStackNavigator />
+    </NavigationContainer>
   );
 }
